ac11064e01a97d1016d8b4a8d3801242d374b126
Godoy Océane <oceane.godoy@enseirb-matmeca.fr>
2020-10-26 16:59:12 +0100

    cors pb

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const port = 3000 || process.env.PORT
 const app = express();
 
 app.listen(port, ()=> {console.log('Server listening on port '+ port);});
+app.use(cors);
 
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été reçue !' }); 
