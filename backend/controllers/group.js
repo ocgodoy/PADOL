0b885558fca792c0ba3683fe7e5a80feba7efcf8
Countermatt <matthieu.pgo@orange.fr>
2021-01-14 15:47:42 +0100

    mise en place ajout utilisateur groupe

--- backend/controllers/group.js
+++ backend/controllers/group.js
@@ -19,7 +19,7 @@ exports.loadGroupsById = (req, res, next, id) => {
     )
 }
 
-exports.inviteGroup = (req, res, next) => { // Demande d'amis
+exports.inviteGroup = (req, res, next) => { // Demande d'ajout au group
   console.log("Invite Group appelé \n")
   console.log("Inviter id " + req.body.userId)
   const inviterGroups = req.group
@@ -36,20 +36,18 @@ exports.inviteGroup = (req, res, next) => { // Demande d'amis
   res.status(200)
 }
 
-exports.addGoupeUser = (req, res, next) => { // Accepter ou refuser demande d'amis
-  console.log("Add Friend appelé status " + req.body.status + "Demander " + req.body.userId + "req group" + JSON.stringify(req.group) +"\n")
-  const accepter = req.group.userId // Celui qui accepte ou refuse l'invitation d'amis
+exports.addGoupeUser = (req, res, next) => { // Accepter ou refuser demande d'ajout au group
+  console.log("Add GroupUser appelé status " + req.body.status + "Demander " + req.body.userId + "req group" + JSON.stringify(req.group) +"\n")
+  const accepter = req.group.userId // Celui qui accepte ou refuse l'invitation au group
   const inviter = req.body.userId // Celui qui a envoyé l'invitation
 
-    Groups.findOneAndUpdate( // Ajout dans la liste d'amis de l'inviteur
+    Groups.findOneAndUpdate( // Ajout dans le group de l'inviteur
       {userId: ObjectId(inviter)},
-      //{$delete: { requests: { userId: accepter} } },
       {$push: { info: { groupUser: accepter} } },
       { new: true }
     ).catch(error => {res.status(401).json({error: error})})
-    Groups.findOneAndUpdate( // Ajout dans la liste d'amis de l'accepteur
+    Groups.findOneAndUpdate( // Ajout dans le group de l'accepteur
       {userId: ObjectId(accepter)},
-      //{$delete: { requests: { userId: inviter} } },
       {$push: { info: { groupUser:ObjectId(inviter)} } },
       { new: true }
     ).catch(error => {res.status(401).json({error: error})})
